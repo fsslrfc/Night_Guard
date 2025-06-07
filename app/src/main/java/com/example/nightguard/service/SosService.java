5d9a85fb37f91a640beaae7431d5ccca5008eca2
fsslrfc <1423676202@qq.com>
2025-06-07 22:40:57 +0800

    [feat]新增SOS服务中获取后台定位内容能力

--- app/src/main/java/com/example/nightguard/service/SosService.java
+++ app/src/main/java/com/example/nightguard/service/SosService.java
@@ -5,16 +5,17 @@ import android.content.Intent;
 import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraManager;
 import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
 import android.os.IBinder;
 import android.util.Log;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
 
 import java.util.Timer;
 import java.util.TimerTask;
@@ -24,18 +25,38 @@ public class SosService extends Service {
     private String cameraId;
     private Timer flashTimer;
     private boolean isTorchOn = false;
-    private LocationManager mLM;
     private String LocationInfo;
-    private LocationListener mLoListener;
+    private LocationCallback mLocationCallback;
     private FusedLocationProviderClient mClient;
 
     @Override
     public void onCreate() {
         super.onCreate();
-
         initLocation();
-        startSos();
+        startFlash();
+        startLocation();
+        startSound();
+        startMessage();
+    }
 
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        stopFlash();
+        stopLocation();
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    private void initLocation() {
+        mClient = LocationServices.getFusedLocationProviderClient(this);
+    }
+
+    private void startFlash() {
         cameraManager = (CameraManager) getSystemService(CAMERA_SERVICE);
 
         try {
@@ -59,11 +80,38 @@ public class SosService extends Service {
         } catch (CameraAccessException e) {
             Log.e("SosService", "相机访问异常", e);
         }
+
     }
 
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
+    private void startLocation() {
+        //TODO
+        LocationRequest locationRequest = LocationRequest.create()
+                .setInterval(5000)
+                .setFastestInterval(2000)
+                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+        mLocationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(@NonNull LocationResult locationResult) {
+                if (locationResult.getLastLocation() != null) {
+                    Location location = locationResult.getLastLocation();
+                    LocationInfo = "精度:" + location.getLongitude()
+                            + "\n纬度:" + location.getLatitude()
+                            + "\n高度:" + location.getAltitude()
+                            + "\n速度:" + location.getSpeed();
+                    Log.d("SosService", "当前位置: " + LocationInfo);
+                }
+            }
+        };
+
+        try {
+            mClient.requestLocationUpdates(locationRequest, mLocationCallback, null);
+        } catch (SecurityException e) {
+            Log.e("SosService", "定位权限不足", e);
+        }
+    }
+
+    private void stopFlash() {
         if (flashTimer != null) {
             flashTimer.cancel();
             flashTimer = null;
@@ -79,37 +127,18 @@ public class SosService extends Service {
         }
     }
 
-    @Nullable
-    @Override
-    public IBinder onBind(Intent intent) {
-        return null;
-    }
-
-    private void initLocation() {
-        mLM = (LocationManager) getSystemService(LOCATION_SERVICE);
-
-        if (!mLM.isProviderEnabled(LocationManager.GPS_PROVIDER) && !mLM.isProviderEnabled(LocationManager.NETWORK_PROVIDER)) {
-            Intent intent = new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            SosService.this.startActivity(intent);
-            Toast.makeText(SosService.this, "请开启定位权限!", Toast.LENGTH_LONG).show();
-        } else {
-             mLoListener = new LocationListener() {
-                @Override
-                public void onLocationChanged(@NonNull Location location) {
-                    LocationInfo = "精度:" + location.getLongitude()
-                            + "\n纬度:" + location.getLatitude()
-                            + "\n高度:" + location.getAltitude()
-                            + "\n速度:" + location.getSpeed();
-                }
-            };
-
-
+    private void stopLocation() {
+        if (mClient != null) {
+            mClient.removeLocationUpdates(mLocationCallback);
         }
-
     }
 
-    private void startSos() {
+    private void startSound() {
+        //TODO
+    }
+
+    private void startMessage() {
+        //TODO
 
 
     }
